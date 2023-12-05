4ccc41ac2585d81b2f3877926dbd9667a3b6845a
Wilmer Pocaterra <malgein17@gmail.com>
2023-12-05 17:11:09 -0400

    feat/hamburger menu responsive added

--- client/src/components/NewContainer.jsx
+++ client/src/components/NewContainer.jsx
@@ -16,11 +16,4 @@ const NewContainer = () => {
 
 export default NewContainer
 
-// Hydrogen VS Electric Cars
-// Will hydrogen-fueled cars ever catch up to EVs?
 
-// The Downsides of AI Artistry
-// What are the possible adverse effects of on-demand AI image generation?
-
-// Is VC Funding Drying Up?
-// Private funding by VC firms is down 50% YOY. We take a look at what that means.
